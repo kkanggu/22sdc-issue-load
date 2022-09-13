9be40ddbaa56c2932ae15aef5a0a3dae6c1acb82
Jin <thinkjin99@gmail.com>
2022-09-14 00:58:29 +0900

    Refactor upload script
    업로드 하는 방식을 함수로 묶어서 스크래핑 코드에서 한번에 실행하는 구조로 변경.
    스크래핑 -> 파일 저장 -> 파일 읽기 -> 업로드 에서
    스크래핑 -> 파일 저장 -> 업로드의 순서로 동작하게 구성.

--- upload_json.py
+++ upload_json.py
@@ -1,45 +1,10 @@
-import boto3
-import os
 import datetime
-import my_logger
-
-def get_last_modified_date(bucket):
-    # last_update_date = bucket['Contents'][-1]['LastModified'] + AWS_TIMEGAP
-    last_update_date = bucket['Contents'][-1]['LastModified']
-    # last_update_date = last_update_date.replace(tzinfo=datetime.timezone.utc)
-    return last_update_date
-
-
-def get_last_file_date(file_name):
-    file_date =  datetime.datetime.strptime(file_name.split('.')[0], "%Y-%m-%d %H:%M")
-    file_date = file_date.replace(tzinfo=datetime.timezone.utc)
-    return file_date
-
+import boto3
 
 #convert file date to utc
-if __name__ == '__main__':
-    JSON_PATH = "json/"
-    logger = my_logger.create_logger()
-    #AWS is utc, local file is in seoul time zone
-    # AWS_TIMEGAP = datetime.timedelta(hours=9)
-    try:
-        s3 = boto3.client('s3', region_name='ap-northeast-2')
-        bucket_name = "issue-keyword-storage"
-        bucket = s3.list_objects_v2(Bucket=bucket_name, Prefix= JSON_PATH, Delimiter='/')
-    
-    except Exception as e:
-        logger.error(e)
-
-    json_files = os.listdir(JSON_PATH)
-    file_name = json_files[-1]
-    
-    last_update_date = get_last_modified_date(bucket)
-    file_date = get_last_file_date(file_name)
-
-    if last_update_date < file_date:
-        file_path = f"{JSON_PATH}/{file_name}"
-        key = "json/" + file_name
-        s3.upload_file(file_path, bucket_name, key)
-    
-    else:
-        print("No need to update")
+def upload_json(file_name):
+    s3 = boto3.client('s3', region_name='ap-northeast-2')
+    bucket_name = "issue-keyword-storage"
+    now = datetime.datetime.now()
+    key = "json/" + datetime.datetime.strftime(now, "%Y-%m-%d %H:%M") + ".json"
+    s3.upload_file(file_name, bucket_name, key)
