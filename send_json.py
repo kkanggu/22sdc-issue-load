f771831ce7ccea16219e36530414e1b421b10671
Jin <thinkjin99@gmail.com>
2022-09-16 00:05:25 +0900

    Add send json to service api
    서비스 api로 모델에게서 전달 받은 키워드 json을 전송합니다.

--- send_json.py
+++ send_json.py
@@ -1,19 +1,28 @@
 import json
 import pathlib
+import datetime
 import requests
 import my_logger
 
 
-def send_json_to_model():
+def send_json_to_model(model_json_file):
     #latest file 10
-    with open(JSON_PATH.joinpath('data_to_model.json')) as json_file:
-        model_json = json.load(json_file)
-
     model_api = "http://10.0.2.208:8000/keyword"
     try:
-        res = requests.post(model_api, json=model_json)
+        res = requests.post(model_api, json=model_json_file)
         assert res.status_code == 200,  "Requset Failed check out header or request parameter"
-        print(res.text)
+        return res
+
+    except Exception as e:
+        logger.warning(e)
+
+
+def send_json_to_service(json_data):
+    service_api = 'http://10.0.2.208:8080/service/modeling'
+    try:
+        res = requests.post(service_api, json=json_data)
+        assert res.status_code == 200,  "Requset Failed check out header or request parameter"
+        return res
 
     except Exception as e:
         logger.warning(e)
@@ -22,5 +31,14 @@ def send_json_to_model():
 if __name__ == '__main__':
     logger = my_logger.create_logger('Api')
     JSON_PATH = pathlib.Path(__file__).parent.joinpath("./json/")
-    send_json_to_model()
 
+    with open(JSON_PATH.joinpath('data_to_model.json'), "r") as json_file:
+        model_json_file = json.load(json_file)
+
+    json_data = send_json_to_model(model_json_file).json()
+    
+    now_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    json_data['date'] = now_date
+    print(json_data)
+
+    send_json_to_service(json_data)
