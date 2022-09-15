3a38c50af035fd39fb682ac9f0961d91ea9bc876
Jin <thinkjin99@gmail.com>
2022-09-15 18:34:05 +0900

    Refactor function flow logic
    함수에서 json을 생성하고 이를 json 폴더에 보관한다.
    기존과 달리 json 데이터를 생성하자마자 전송하지 않는다.

--- control_s3.py
+++ control_s3.py
@@ -1,18 +1,15 @@
 import datetime
 import json
+import pathlib
 import boto3
-import requests
 import my_logger
 
-s3 = boto3.client('s3', region_name='ap-northeast-2')
-BUCKET_NAME = "issue-keyword-storage"
-logger = my_logger.create_logger()
-
-def upload_json(file_path):
+def upload_json():
     try:
         now = datetime.datetime.now()
         key = "json/" + datetime.datetime.strftime(now, "%Y-%m-%d %H:%M") + ".json"
-        s3.upload_file(file_path, BUCKET_NAME, key)
+        s3.upload_file(str(FILE_PATH), BUCKET_NAME, key)
+        logger.info("Upload json to S3!")
     
     except Exception as e:
         logger.warning(e)
@@ -25,21 +22,21 @@ def create_json_to_send():
         json_objects = [json.loads(obj_dict['Body'].read().decode()) for obj_dict in s3_objects]
         title_contents = [article['title'] + " " + article['content'] for j_obj in json_objects for article in j_obj]
         model_json = {'articles': title_contents}
-        return model_json
 
-    except Exception as e:
-        logger.warning(e)
-
-
-def send_json_to_model():
-    #latest file 10
-    model_json = create_json_to_send()
-    model_api = "http://10.0.2.208:8000/keyword"
-    try:
-        res = requests.post(model_api, json=model_json)
-        assert res.status_code == 200,  "Requset Failed check out header or request parameter"
-        print(res.text)
-
-    except Exception as e:
-        logger.warning(e)
+        with open(JSON_PATH.joinpath('data_to_model.json'), "w") as json_file:
+            json.dump(model_json, json_file, indent=4)
         
+        logger.info("Create json file to send model.")
+
+    except Exception as e:
+        logger.warning(e)
+
+
+if __name__ == "__main__":
+    s3 = boto3.client('s3', region_name='ap-northeast-2')
+    BUCKET_NAME = "issue-keyword-storage"
+    JSON_PATH = pathlib.Path(__file__).parent.joinpath("./json/")
+    FILE_PATH = JSON_PATH.joinpath("last_scrapped.json")
+    logger = my_logger.create_logger('S3')
+    upload_json()
+    create_json_to_send()
